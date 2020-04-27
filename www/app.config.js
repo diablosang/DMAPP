a72b6f4ac9a5d06ce85fab9345abbcff8ce16817
Klaus Sang <13344301+diablosang@users.noreply.github.com>
2020-04-27 11:42:27 +0800

    1.37.2

--- www/app.config.js
+++ www/app.config.js
@@ -1,7 +1,7 @@
 // NOTE object below must be a valid JSON
 var appStartView = "WorkShop2";
 var keepPopUserInfo = true;
-var appVer = "1.37.1";
+var appVer = "1.37.2";
 var dbProfile = "JGBR";
 var asUserList = [];
 var asRoles = [];
